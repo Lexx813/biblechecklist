eece4836884431f00900e4dc219a955fb0e03c8a
User <user@example.com>
2026-03-31 20:05:44 -0500

    redesign nav

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -97,7 +97,7 @@ export default function HomePage({ user, navigate, onLogout, darkMode, setDarkMo
   return (
     <div className="home-wrap">
 
-      <PageNav navigate={navigate} darkMode={darkMode} setDarkMode={setDarkMode} i18n={i18n} user={user} onLogout={onLogout}  onUpgrade={onUpgrade}/>
+      <PageNav navigate={navigate} darkMode={darkMode} setDarkMode={setDarkMode} i18n={i18n} user={user} onLogout={onLogout} currentPage="home" onUpgrade={onUpgrade}/>
 
       {/* ── Hero ── */}
       <main id="main-content">
