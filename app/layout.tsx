4506728e13a5b12a6559b757110f002ca6e7bff8
User <user@example.com>
2026-04-07 16:08:28 -0500

    Revert "css"
    This reverts commit d71103badc55b8a0d6b06115abf011e22f06bc50.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,7 @@
 import Script from "next/script";
 import Providers from "./providers";
-// @ts-ignore - CSS side-effect imports (rendered as render-blocking <link> in the initial HTML to prevent FOUC)
+// @ts-ignore - CSS side-effect import
 import "../src/styles/app.css";
-// @ts-ignore
-import "../src/styles/app-layout.css";
-// @ts-ignore
-import "../src/styles/topbar.css";
-// @ts-ignore
-import "../src/styles/home.css";
-// @ts-ignore
-import "../src/styles/landing.css";
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
